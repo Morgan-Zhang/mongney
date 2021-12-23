6c1129cddf77bf9a71b559814ad58abdd087a9fc
morgan <zhangteng_yx@126.com>
2021-12-23 10:43:26 +0800

    引入svg

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,69 +1,12 @@
 <template>
-  <div class="nav-wrapper">
-   <div class="content">
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-     <div>Money.vue</div>
-   </div>
-    <Nav/>
-  </div>
+
+
+    <Layout>
+      <p>Money.vue</p>
+    </Layout>
+
+
+
 </template>
 
 <script lang="ts">
@@ -72,16 +15,3 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.nav-wrapper{
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content{
-  border: 1px solid blue;
-  overflow: auto;
-  flex-grow: 1;
-}
-</style>
