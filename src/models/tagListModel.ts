e2e50adea9ced4cd9f04c2fa34965d83ff2ae0c9
morgan <zhangteng_yx@126.com>
2022-01-07 21:13:46 +0800

    删除和回退按钮有bug

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,13 +1,14 @@
 const localStorageKeyName = 'tagList';
 type Tag = {
-  id: string,
-  name: string,
+  id: string;
+  name: string;
 }
 type TagListModel = {
   data: Tag[]
   fetch: () => Tag[]
   create: (name: string) => 'success' | 'duplicated' // 联合类型
   update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+  remove: (id: string) => boolean
   save: () => void
 }
 const tagListModel: TagListModel = {
@@ -17,6 +18,7 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name) {
+    // this.data = [{id:'1', name:'1'}, {id:'2', name:'2'}]
     const names = this.data.map(item => item.name);
     if (names.indexOf(name) >= 0) {return 'duplicated';}
     this.data.push({id: name, name: name});
@@ -39,7 +41,18 @@ const tagListModel: TagListModel = {
       return 'not found';
     }
   },
-
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
+  },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
